eb5e37ed495551da008a46a1f2e90f271af8f254
iZanfir <izanfir7@gmail.com>
2021-06-26 16:49:40 -0300

    Other: test in your own machine

---
+++ example/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import AutoTyping, { BlinkCursor } from 'react-auto-typing'
+
+const App = () => (
+  <>
+    <AutoTyping />
+    <BlinkCursor />
+  </>
+)
+
+export default App
